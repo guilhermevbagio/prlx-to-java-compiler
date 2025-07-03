ea2d89862f9971e8505a7d4db878f9b4f5389cfa
guilherme <guilhermevbagio@gmail.com>
2025-07-03 11:37:25 -0300

    package, main parametrizada, testes e correção de sintaxe do sable

---
+++ src/prolixa/Main.java
@@ -0,0 +1,38 @@
+package prolixa;
+import prolixa.lexer.*;
+import prolixa.node.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			File folder = new File("test");
+			File[] files = folder.listFiles();
+			
+			for(File file : files) {
+				
+				String arquivo = file.getName();
+
+				Lexer lexer =
+						new Lexer(
+								new PushbackReader(  
+										new FileReader(arquivo), 1024)); 
+				Token token;
+				
+				System.out.println("-------------" + file.getName().toUpperCase() + "-------------");
+				
+				while(!((token = lexer.next()) instanceof EOF)) {
+					System.out.println(token.getClass());
+					System.out.println(" ( "+token.toString()+")");
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
